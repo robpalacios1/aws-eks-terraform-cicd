00baba1ed7aa268d5c27fe836325a21e4da8f1a6
roberto <robpalacios1@hotmail.com>
2026-05-13 11:44:50 -0500

    Update availability zone on Public Subnets 2

--- modules/infrastructure/main.tf
+++ modules/infrastructure/main.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "public-subnet-1" {
 resource "aws_subnet" "public-subnet-2" {
     vpc_id = aws_vpc.main-vpc
     cidr_block = "10.0.2.0/24"
-    availability_zone = "us-east-2a"
+    availability_zone = "us-east-1b"
     map_public_ip_on_launch = true
 
     tags = {
