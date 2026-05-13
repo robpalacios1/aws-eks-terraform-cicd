eaafa6cf1432db4f21ca8598db0651fa97d52e08
roberto <robpalacios1@hotmail.com>
2026-05-13 16:58:52 -0500

    Update vpc_id

--- modules/infrastructure/main.tf
+++ modules/infrastructure/main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main-vpc" {
 
 # 2. Create Public Subnets (1)
 resource "aws_subnet" "public-subnet-1" {
-    vpc_id = aws_vpc.main-vpc
+    vpc_id = aws_vpc.main-vpc.id
     cidr_block = "10.0.1.0/24"
     availability_zone = "us-east-1a"
     map_public_ip_on_launch = true
@@ -21,7 +21,7 @@ resource "aws_subnet" "public-subnet-1" {
 
 # 3. Create Public Subnet (2)
 resource "aws_subnet" "public-subnet-2" {
-    vpc_id = aws_vpc.main-vpc
+    vpc_id = aws_vpc.main-vpc.id
     cidr_block = "10.0.2.0/24"
     availability_zone = "us-east-1b"
     map_public_ip_on_launch = true
@@ -33,21 +33,21 @@ resource "aws_subnet" "public-subnet-2" {
 
 # 4. Create Private Subnet (1)
 resource "aws_subnet" "private-subnet-1" {
-  vpc_id = aws_vpc.main-vpc
+  vpc_id = aws_vpc.main-vpc.id
   cidr_block = "10.0.3.0/24"
   availability_zone = "us-east-1a"
 }
 
 # 5. Create Private Subnet (2)
 resource "aws_subnet" "private-subnet-2" {
-  vpc_id = aws_vpc.main-vpc
+  vpc_id = aws_vpc.main-vpc.id
   cidr_block = "10.0.4.0/24"
   availability_zone = "us-east-1b"
 }
 
 # 6. Create Internet Gateway
 resource "aws_internet_gateway" "main-igw" {
-  vpc_id = aws_vpc.main-vpc
+  vpc_id = aws_vpc.main-vpc.id
 
   tags = {
     Name = "main-igw"
