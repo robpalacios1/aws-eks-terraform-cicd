319ac4a1385cfc80b94e995003336594f7ad80e9
roberto <robpalacios1@hotmail.com>
2026-05-13 17:51:22 -0500

    Create Route

--- modules/infrastructure/main.tf
+++ modules/infrastructure/main.tf
@@ -109,3 +109,10 @@ resource "aws_route_table" "public-rt" {
     environment = "dev"
   }
 }
+
+# 12. Create Route
+resource "aws_route" "public-default-igw" {
+  route_table_id = aws_route_table.public-rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main-igw.id
+}
