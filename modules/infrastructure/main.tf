b1d9138e52692098a521b201bbaa22f7f4625d2e
roberto <robpalacios1@hotmail.com>
2026-05-13 11:19:56 -0500

    Create Public Subnets for infrastructure

--- modules/infrastructure/main.tf
+++ modules/infrastructure/main.tf
@@ -6,3 +6,27 @@ resource "aws_vpc" "main-vpc" {
         environment = "dev"
     }
 }
+
+# 2. Create Public Subnets (1)
+resource "aws_subnet" "public-subnet-1" {
+    vpc_id = aws_vpc.main-vpc
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
+    
+    tags = {
+      Name = "public-subnet-1"
+    }
+}
+
+# 3. Create Public Subnet (2)
+resource "aws_subnet" "public-subnet-2" {
+    vpc_id = aws_vpc.main-vpc
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "us-east-2a"
+    map_public_ip_on_launch = true
+
+    tags = {
+      Name = "public-subnet-2"
+    }
+}
