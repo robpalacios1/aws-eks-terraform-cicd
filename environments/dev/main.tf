c308c4bdf577c5cf98627396d8bc507724bff4f5
roberto <robpalacios1@hotmail.com>
2026-05-14 16:54:36 -0500

    formate code with terraform fmt

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,57 +1,57 @@
 module "networking" {
   source = "../../modules/networking"
 
-# variables for networking module 
+  # variables for networking module 
 
-# VPC
-  vpc_cidr_block = "10.0.0.0/16"
-  vpc_name = "development"
+  # VPC
+  vpc_cidr_block  = "10.0.0.0/16"
+  vpc_name        = "development"
   vpc_environment = "dev"
 
-# public Subnet 1
+  # public Subnet 1
   public_subnet_1_cidr_block = "10.0.1.0/24"
-  public_subnet_1_az1a = "us-east-1a"
-  public_subnet_1_name = "public-subnet-1"
+  public_subnet_1_az1a       = "us-east-1a"
+  public_subnet_1_name       = "public-subnet-1"
 
-# public Subnet 2
+  # public Subnet 2
   public_subnet_2_cidr_block = "10.0.2.0/24"
-  public_subnet_2_az1b = "us-east-1b"
-  public_subnet_2_name = "public-subnet-2"
+  public_subnet_2_az1b       = "us-east-1b"
+  public_subnet_2_name       = "public-subnet-2"
 
-# private Subnet 1
+  # private Subnet 1
   private_subnet_1_cidr_block = "10.0.3.0/24"
-  private_subnet_1_az1a = "us-east-1a"
-  private_subnet_1_name = "private-subnet-1"
+  private_subnet_1_az1a       = "us-east-1a"
+  private_subnet_1_name       = "private-subnet-1"
 
-# private Subnet 2
+  # private Subnet 2
   private_subnet_2_cidr_block = "10.0.4.0/24"
-  private_subnet_2_az1b = "us-east-1b"
-  private_subnet_2_name = "private-subnet-2"
+  private_subnet_2_az1b       = "us-east-1b"
+  private_subnet_2_name       = "private-subnet-2"
 
-# Internet Gatewy
+  # Internet Gatewy
   igw_name = "main-igw"
 
-# Elastic IP (1)
+  # Elastic IP (1)
   nat_eip_az1_name = "nat-eip-az1"
 
-# Elastic IP (2)
+  # Elastic IP (2)
   nat_eip_az2_name = "nat-eip-az2"
 
-# NAT Gateway AZ1
+  # NAT Gateway AZ1
   nat_gw_az1_name = "nat-gw-az1"
 
-# NAT Gateway AZ2
+  # NAT Gateway AZ2
   nat_gw_az2_name = "nat-gw-az2"
 
-# Public Route Table
+  # Public Route Table
   public_rt_name = "public-rt"
 
-# Destination cidr block
+  # Destination cidr block
   public_destination_cidr_block = "0.0.0.0/0"
 
-# Private Route Table AZ1
+  # Private Route Table AZ1
   private_rt_az1_name = "private-rt-az1"
 
-# Private Route Table AZ2
+  # Private Route Table AZ2
   private_rt_az2_name = "private-rt-az2"
 }
