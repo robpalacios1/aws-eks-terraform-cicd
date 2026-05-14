e5bc3348b0f21a53b6c36c545e16e393214ff0a0
roberto <robpalacios1@hotmail.com>
2026-05-14 16:28:04 -0500

    create outputs in outputs.tf file

---
+++ modules/networking/outputs.tf
@@ -0,0 +1,23 @@
+# VPC ID
+output "vpc_id" {
+  description = "VPC ID"
+  value = aws_vpc.main-vpc.id
+}
+
+# Public Subnets list ID
+output "public_subnet_ids" {
+  description = "Public Subnets list ID"
+  value = [
+    aws_subnet.public-subnet-1.id,
+    aws_subnet.public-subnet-2.id
+  ]
+}
+
+# Private Subnets list ID
+output "private_subnet_ids" {
+  description = "Private Subnets list ID"
+  value = [
+    aws_subnet.private-subnet-1.id,
+    aws_subnet.private-subnet-2.id
+  ]
+}
