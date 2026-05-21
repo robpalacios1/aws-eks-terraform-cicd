8c077f0539f93a103d00dbc3798764ca917f11a5
roberto <robpalacios1@hotmail.com>
2026-05-21 15:33:08 -0500

    add variables to Node Groups

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -85,8 +85,10 @@ module "eks" {
   # Node Groups
   main_eks_node_group_name = "development-eks-node-group"
   main_eks_node_group_subnets_ids = module.networking.private_subnet_ids
-  main_eks_node_group_desired_size = 3
-  main_eks_node_group_max_size = 3
+  main_eks_node_group_capacity_type = "ON_DEMAND"
+  main_eks_node_group_instance_type = ["t3.micro"]
+  main_eks_node_group_desired_size = 2
+  main_eks_node_group_max_size = 2
   main_eks_node_group_min_size = 1
   main_eks_node_group_max_unavailable = 1
 }
