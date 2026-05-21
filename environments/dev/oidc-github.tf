4258f3f13e689118c488fe0665f09a65659c8970
roberto <robpalacios1@hotmail.com>
2026-05-21 18:39:20 -0500

    update stringLike

--- environments/dev/oidc-github.tf
+++ environments/dev/oidc-github.tf
@@ -32,7 +32,7 @@ resource "aws_iam_role" "github_actions_role" {
                     "token.actions.githubusercontent.com:aud" = "sts:amazonaws.com"
                 },
                 "StringLike" = {
-                    "token.actions.githubusercontent.com:sub" = "repo:robpalacios1/aws-eks-terraform-cicd/tree/feature/development"
+                    "token.actions.githubusercontent.com:sub" = "repo:robpalacios1/aws-eks-terraform-cicd:*"
                 }
             }
         }
