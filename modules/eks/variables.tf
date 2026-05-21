6273e54ea2a5c5762c1575f691a004424aa1dd86
roberto <robpalacios1@hotmail.com>
2026-05-21 16:03:12 -0500

    indent code with terraform fmt command

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -18,14 +18,14 @@ variable "node_name" {
 
 variable "app_ecr_repo_name" {
   description = "Name of ECR Repo on development environment"
-  type = string 
-  default = "development-app-ecr-repo"
+  type        = string
+  default     = "development-app-ecr-repo"
 }
 
 variable "app_ecr_image_mutability" {
   description = "Image mutability"
-  type = string 
-  default = "MUTABLE"
+  type        = string
+  default     = "MUTABLE"
 }
 
 variable "app_ecr_environment" {
@@ -39,31 +39,31 @@ variable "app_ecr_environment" {
 
 variable "main_eks_cluster_name" {
   description = "Name of EKS Cluster on development environment"
-  type = string
-  default = "development-eks-cluster"
+  type        = string
+  default     = "development-eks-cluster"
 }
 
 variable "main_eks_cluster_version" {
   description = "Version EKS Cluster"
-  type = string
-  default = "1.35"
+  type        = string
+  default     = "1.35"
 }
 
 variable "main_eks_cluster_subnets_ids" {
   description = "Subnets ID's"
-  type = list(string)
+  type        = list(string)
 }
 
 variable "main_eks_cluster_public_access" {
   description = "Public Access"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "main_eks_cluster_private_access" {
   description = "Public Access"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 //////////////////////////////////////////////////////
@@ -71,47 +71,47 @@ variable "main_eks_cluster_private_access" {
 
 variable "main_eks_node_group_name" {
   description = "Name of EKS Node Group on development environment"
-  type = string
-  default = "development-eks-node-group"
+  type        = string
+  default     = "development-eks-node-group"
 }
 
 variable "main_eks_node_group_subnets_ids" {
   description = "Subnets ID's"
-  type = list(string)
+  type        = list(string)
 }
 
 variable "main_eks_node_group_capacity_type" {
   description = "Capacity Type"
-  type = string
-  default = "ON_DEMAND"
+  type        = string
+  default     = "ON_DEMAND"
 }
 
 variable "main_eks_node_group_instance_type" {
   description = "Instance Type"
-  type = list(string)
-  default = ["t3.micro"]
+  type        = list(string)
+  default     = ["t3.micro"]
 }
 
 variable "main_eks_node_group_desired_size" {
   description = "Scaling Config in desired Size"
-  type = number
-  default = 2
+  type        = number
+  default     = 2
 }
 
 variable "main_eks_node_group_max_size" {
   description = "Scaling Config in Max Size"
-  type = number
-  default = 2
+  type        = number
+  default     = 2
 }
 
 variable "main_eks_node_group_min_size" {
   description = "Scaling Config in Min Size"
-  type = number
-  default = 1
+  type        = number
+  default     = 1
 }
 
 variable "main_eks_node_group_max_unavailable" {
   description = "Time of inactivity"
-  type = number
-  default = 1
+  type        = number
+  default     = 1
 }
