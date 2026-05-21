21f017738114fbd6defd8c9dd703971b9579db80
roberto <robpalacios1@hotmail.com>
2026-05-21 18:25:54 -0500

    Update repo link incluided in feature/*

--- environments/dev/oidc-github.tf
+++ environments/dev/oidc-github.tf
@@ -29,7 +29,7 @@ resource "aws_iam_role" "github_actions_role" {
                     "token.actions.githubusercontent.com:aud" = "sts:amazonaws.com"
                 },
                 "StringLike" = {
-                    "token.actions.githubusercontent.com:sub" = "repo:robpalacios1/aws-eks-terraform-cicd"
+                    "token.actions.githubusercontent.com:sub" = "repo:robpalacios1/aws-eks-terraform-cicd/tree/feature/development"
                 }
             }
         }
