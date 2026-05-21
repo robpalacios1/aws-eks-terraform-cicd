74d8f45b68e390eaa5767fc91817bf7c85750e03
roberto <robpalacios1@hotmail.com>
2026-05-21 18:31:00 -0500

    update thumbprint list

--- environments/dev/oidc-github.tf
+++ environments/dev/oidc-github.tf
@@ -5,7 +5,10 @@
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url = "https://token.actions.githubusercontent.com"
   client_id_list = [ "sts.amazonaws.com" ]
-  thumbprint_list = [ "1b511abead59c6ce207077c0bf0e0043b1382612" ]
+  thumbprint_list = [ 
+    "1b511abead59c6ce207077c0bf0e0043b1382612", 
+    "6938fd4d98bab03faadb97b34396831e3780aea1" 
+  ]
 }
 
 # ====================================================================
