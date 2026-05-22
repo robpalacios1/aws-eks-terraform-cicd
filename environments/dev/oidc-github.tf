7c4e61e379149167230f94f7782fcb986e3af545
roberto <robpalacios1@hotmail.com>
2026-05-21 19:04:55 -0500

    Fix error in condition StringEquals

--- environments/dev/oidc-github.tf
+++ environments/dev/oidc-github.tf
@@ -29,7 +29,7 @@ resource "aws_iam_role" "github_actions_role" {
             Action = "sts:AssumeRoleWithWebIdentity",
             Condition = {
                 "StringEquals" = {
-                    "token.actions.githubusercontent.com:aud" = "sts:amazonaws.com"
+                    "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
                 },
                 "StringLike" = {
                     "token.actions.githubusercontent.com:sub" = "repo:robpalacios1/aws-eks-terraform-cicd:*"
